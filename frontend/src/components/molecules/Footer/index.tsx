d8d81fdbbd4129efbd73b77028ddb08bf5a9139e
aritrazomoso <127817946+aritrazomoso@users.noreply.github.com>
2023-09-29 12:44:58 +0530

    Fix Added footer style (#12)
    Co-authored-by: Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>

--- frontend/src/components/molecules/Footer/index.tsx
+++ frontend/src/components/molecules/Footer/index.tsx
@@ -8,56 +8,71 @@ import {
   NEED_HELP,
 } from '@/strings/constant'
 import theme from '@/theme'
-import { Stack, StackProps, Typography } from '@mui/material'
+import { Box, BoxProps, Stack, Typography } from '@mui/material'
 
-interface FooterProps extends StackProps {}
+interface FooterProps extends BoxProps {
+  width?: BoxProps['width']
+}
 
-const Footer = ({ ...props }: FooterProps) => {
+const Footer = ({
+  width = `calc(100% - ${theme.spacing(12)})`,
+  ...props
+}: FooterProps) => {
   return (
-    <Stack {...props} direction="row" justifyContent="space-between">
-      <Stack direction="row" alignItems="center" gap={6}>
-        <ButtonComponent
-          typographyVarient="body2"
-          label={DASHBOARD}
-          variant="text"
-          textColor={theme.palette.primary[500]}
-        />
-        <ButtonComponent
-          typographyVarient="body2"
-          label={CAREERS}
-          variant="text"
-          textColor={theme.palette.primary[500]}
-        />
-        <ButtonComponent
-          typographyVarient="body2"
-          label={LEGAL_AND_PRIVACY}
-          variant="text"
-          textColor={theme.palette.primary[500]}
-        />
-        <Typography variant="body2">{MINET_TRADEMARK}</Typography>
+    <Box
+      component="footer"
+      position="fixed"
+      left={theme.spacing(6)}
+      bottom={theme.spacing(6)}
+      mr={6}
+      width={width}
+      {...props}
+    >
+      <Stack direction="row" justifyContent="space-between">
+        <Stack direction="row" alignItems="center" gap={6}>
+          <ButtonComponent
+            typographyVarient="body2"
+            label={DASHBOARD}
+            variant="text"
+            textColor={theme.palette.primary[500]}
+          />
+          <ButtonComponent
+            typographyVarient="body2"
+            label={CAREERS}
+            variant="text"
+            textColor={theme.palette.primary[500]}
+          />
+          <ButtonComponent
+            typographyVarient="body2"
+            label={LEGAL_AND_PRIVACY}
+            variant="text"
+            textColor={theme.palette.primary[500]}
+          />
+          <Typography variant="body2">{MINET_TRADEMARK}</Typography>
+        </Stack>
+        <Stack direction="row" gap={6}>
+          <Dropdown size="small" padding={theme.spacing(0)}>
+            {[
+              <Typography
+                key={1}
+                variant="body2"
+                color={theme.palette.text.highEmphasis}
+              >
+                English
+              </Typography>,
+            ]}
+          </Dropdown>
+          <ButtonComponent
+            label={NEED_HELP}
+            variant="outlined"
+            textColor={theme.palette.primary[500]}
+            sx={{
+              width: '100%',
+            }}
+          />
+        </Stack>
       </Stack>
-      <Stack direction="row" gap={6}>
-        <Dropdown size="small" padding={theme.spacing(0)}>
-          {[
-            <Typography
-              key={1}
-              variant="body2"
-              color={theme.palette.text.highEmphasis}
-            >
-              English
-            </Typography>,
-          ]}
-        </Dropdown>
-        <ButtonComponent
-          label={NEED_HELP}
-          variant="outlined"
-          textColor={theme.palette.primary[500]}
-          sx={{
-            width: '100%',
-          }}
-        />
-      </Stack>
-    </Stack>
+    </Box>
   )
 }
 

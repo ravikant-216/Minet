1b823a75c911da3bf6db7d77dd36d24e4dcc600c
Ashutosh Pandey <124673637+ashutoshpandey3142@users.noreply.github.com>
2023-10-17 17:40:21 +0530

    Be feat/bc123 91 flyway integration (#84)
    * BE_feat/BC-123-91 Added flyway setup
    
    ---------
    
    Co-authored-by: Ashutosh Pandey <ashutosh.pandey@zemosolab.com>

---
+++ backend/Portfolio-Service/src/main/resources/db/migration/V5__create_wallet.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `minet`.`wallet` (
+  `id` binary(16)  NOT NULL,
+  `user_id` binary(16)  NOT NULL,
+  `crypto_id` binary(16)  NOT NULL,
+  `total_balance` decimal(65,3) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `wallet_user_id_fkey` (`user_id`),
+  KEY `wallet_crypto_id_fkey` (`crypto_id`),
+  CONSTRAINT `wallet_crypto_id_fkey` FOREIGN KEY (`crypto_id`) REFERENCES `crypto` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `wallet_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ;
--- backend/User-Service/src/main/resources/db/migration/V1__create_user_table.sql
+++ backend/User-Service/src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `minet`.`user` (
-  `id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` binary(16)  NOT NULL,
+  `name` varchar(100)  NOT NULL,
+  `email` varchar(100)  NOT NULL,
+  `password` varchar(100)  NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);

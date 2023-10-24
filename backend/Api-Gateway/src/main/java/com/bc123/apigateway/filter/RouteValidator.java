e1454890db994963b4a6122933dd3207286ca95c
Ashutosh Pandey <ashutosh.pandey@zemosolab.com>
2023-10-24 15:25:18 +0530

    feat:BC-123 update transaction path

--- backend/Api-Gateway/src/main/java/com/bc123/apigateway/filter/RouteValidator.java
+++ backend/Api-Gateway/src/main/java/com/bc123/apigateway/filter/RouteValidator.java
@@ -1,6 +1,7 @@
 package com.bc123.apigateway.filter;
 
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 
@@ -9,11 +10,11 @@ import java.util.List;
 import java.util.function.Predicate;
 
 @Component
+@Slf4j
 @NoArgsConstructor
 public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
-            "/user",
-            "/api/v1/wallet/**"
+            "/user"
     );
 
     public static final Predicate<ServerHttpRequest> isSecured =
